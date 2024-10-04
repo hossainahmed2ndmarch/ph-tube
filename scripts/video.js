153984211cfa614b88a1942903d970853bffd35d
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2024-10-04 16:17:03 +0600

    Make a dynamic website

--- scripts/video.js
+++ scripts/video.js
@@ -1,20 +1,110 @@
 // Load catagories
 const loadCategories = () => {
- fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-  .then(res => res.json())
-  .then(data => displayCategories(data.categories))
-  .catch(error => console.log(error));
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+    .catch(error => console.log(error));
 }
 
 
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+    .catch(error => console.log(error));
+}
+
+
+const loadCategoryVideos = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res => res.json())
+    .then(data => displayVideos(data.category))
+    .catch(error => console.log(error))
+}
+
+
+// Display categories
 const displayCategories = (categories) => {
- const categoriesContainer = document.getElementById('categories');
- categories.forEach((items) => {
-  const button = document.createElement("button")
-  button.classList = "btn";
-  button.innerText = items.category;
-  categoriesContainer.append(button);
- });
+  const categoriesContainer = document.getElementById('categories');
+  categories.forEach((items) => {
+    const buttonContainer = document.createElement("div")
+    buttonContainer.innerHTML =
+      `
+     <button onclick="loadCategoryVideos(${items.category_id})" class="btn">
+      ${items.category}
+     </button>
+    `
+    categoriesContainer.append(buttonContainer);
+  });
+}
+
+
+function getTimeString(time) {
+  const hour = parseInt(time / 3600);
+  let remainingSeconds = time % 3600;
+  const minute = parseInt(remainingSeconds / 60);
+  remainingSeconds = remainingSeconds % 60;
+  return `${hour} hour ${minute} minute ${remainingSeconds} ago`
+}
+
+
+const carDemo = {
+  category_id: "1001",
+  video_id: "aaaa",
+  thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
+  title: "Shape of You",
+  authors: [
+    {
+      profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
+      profile_name: "Olivia Mitchell",
+      verified: ""
+    }
+  ],
+  others: {
+    views: "100K",
+    posted_date: "16278"
+  },
+  description: "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}
+
+// Display videos
+const displayVideos = (videos) => {
+  const videosContainer = document.getElementById('videos');
+  videosContainer.innerHTML = "";
+  videos.forEach(video => {
+    console.log(video);
+    const card = document.createElement('div');
+    card.classList = "card card-compact";
+    card.innerHTML = `
+      <figure class="h-[200px] relative">
+      <img
+      src=${video.thumbnail}
+      class="h-full w-full object-cover"
+      alt="Shoes" />
+      ${video.others.posted_date?.length === 0
+        ? ""
+        : `<span class="absolute right-2 bottom-2 text-sm text-white bg-black rounded p-1">
+        ${getTimeString(video.others.posted_date)}
+       </span>`
+      }
+      </figure>
+      <div class="px-0 py-2 flex gap-2">
+        <div>
+         <img class="h-10 w-10 rounded-full object-cover" src=${video.authors[0].profile_picture}/>
+        </div>
+        <div>
+         <h2 class="font-bold">${video.title}</h2>
+         <div class="flex items-center gap-2">
+          <p class="text-gray-400">${video.authors[0].profile_name}</P>
+          ${video.authors[0].verified === true ? `<img class="w-5 object-cover" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png"/>` : ""}
+         </div>
+         <P></P>
+        </div>
+      </div>
+  `;
+    videosContainer.append(card);
+  })
 }
 
 loadCategories();
+loadVideos();
